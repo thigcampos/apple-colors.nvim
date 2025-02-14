d5b87f62a82819ec3acdd68b7cc743036002f254
Thiago Campos <commit@thigcampos.com>
2025-02-14 12:20:08 -0300

    remove all mentions to color-scheme old name and replacing them with cupertino

---
+++ lua/cupertino/integrations/cmp.lua
@@ -0,0 +1,44 @@
+local colorscheme = require("cupertino.colorscheme")
+
+local M = {}
+
+function M.highlights()
+	return {
+		CmpItemAbbr = { fg = colorscheme.mainText },
+		CmpItemAbbrDeprecated = { fg = colorscheme.mainText, strikethrough = true },
+		CmpItemKind = { fg = colorscheme.syntaxFunction },
+		CmpItemMenu = { fg = colorscheme.mainText },
+		CmpItemAbbrMatch = { fg = colorscheme.successText, bold = true },
+		CmpItemAbbrMatchFuzzy = { fg = colorscheme.successText, bold = true },
+
+		-- kind support
+		CmpItemKindSnippet = { fg = colorscheme.stringText },
+		CmpItemKindKeyword = { fg = colorscheme.syntaxError },
+		CmpItemKindText = { fg = colorscheme.warningEmphasis },
+		CmpItemKindMethod = { fg = colorscheme.syntaxKeyword },
+		CmpItemKindConstructor = { fg = colorscheme.syntaxKeyword },
+		CmpItemKindFunction = { fg = colorscheme.syntaxKeyword },
+		CmpItemKindFolder = { fg = colorscheme.syntaxKeyword },
+		CmpItemKindModule = { fg = colorscheme.syntaxKeyword },
+		CmpItemKindConstant = { fg = colorscheme.syntaxKeyword },
+		CmpItemKindField = { fg = colorscheme.syntaxKeyword },
+		CmpItemKindProperty = { fg = colorscheme.successText },
+		CmpItemKindEnum = { fg = colorscheme.successText },
+		CmpItemKindUnit = { fg = colorscheme.successText },
+		CmpItemKindClass = { fg = colorscheme.warningText },
+		CmpItemKindVariable = { fg = colorscheme.syntaxError },
+		CmpItemKindFile = { fg = colorscheme.linkText },
+		CmpItemKindInterface = { fg = colorscheme.stringText },
+		CmpItemKindColor = { fg = colorscheme.syntaxError },
+		CmpItemKindReference = { fg = colorscheme.syntaxError },
+		CmpItemKindEnumMember = { fg = colorscheme.syntaxError },
+		CmpItemKindStruct = { fg = colorscheme.syntaxFunction },
+		CmpItemKindValue = { fg = colorscheme.warningEmphasis },
+		CmpItemKindEvent = { fg = colorscheme.syntaxFunction },
+		CmpItemKindOperator = { fg = colorscheme.syntaxError },
+		CmpItemKindTypeParameter = { fg = colorscheme.syntaxError },
+		CmpItemKindCopilot = { fg = colorscheme.linkText },
+	}
+end
+
+return M
---
+++ lua/lualine/themes/cupertino.lua
@@ -0,0 +1,64 @@
+local colorscheme = require("cupertino.colorscheme")
+local config = require("cupertino.config")
+local theme = {}
+
+local bg = config.transparent and "NONE" or colorscheme.editorBackground
+
+theme.normal = {
+	a = { bg = colorscheme.blue, fg = colorscheme.editorBackground, gui = "bold" },
+	b = { bg = bg, fg = colorscheme.commentText },
+	c = { bg = bg, fg = colorscheme.editorBackground },
+}
+
+theme.insert = {
+	a = {
+		bg = colorscheme.syntaxError,
+		fg = colorscheme.editorBackground,
+		gui = "bold",
+	},
+	b = { bg = bg, fg = colorscheme.commentText },
+}
+
+theme.terminal = {
+	a = {
+		bg = colorscheme.successText,
+		fg = colorscheme.editorBackground,
+		gui = "bold",
+	},
+	b = { bg = bg, fg = colorscheme.commentText },
+}
+
+theme.command = {
+	a = {
+		bg = colorscheme.successText,
+		fg = colorscheme.editorBackground,
+		gui = "bold",
+	},
+	b = { bg = bg, fg = colorscheme.commentText },
+}
+
+theme.visual = {
+	a = {
+		bg = colorscheme.syntaxKeyword,
+		fg = colorscheme.editorBackground,
+		gui = "bold",
+	},
+	b = { bg = bg, fg = colorscheme.commentText },
+}
+
+theme.replace = {
+	a = {
+		bg = colorscheme.warningText,
+		fg = colorscheme.editorBackground,
+		gui = "bold",
+	},
+	b = { bg = bg, fg = colorscheme.commentText },
+}
+
+theme.inactive = {
+	a = { bg = colorscheme.commentText, fg = colorscheme.editorBackground },
+	b = { bg = bg, fg = colorscheme.warningText, gui = "bold" },
+	c = { bg = bg, fg = colorscheme.warningText },
+}
+
+return theme
